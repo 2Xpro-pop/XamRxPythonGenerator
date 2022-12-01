67a2823ad0671a15e939c53384b46b834e373cba
2Xpro-pop <bay.asanov@mail.ru>
2022-12-01 19:07:39 +0600

    bug fixed

---
+++ test/Views/AdminchickPage.cs
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="utf-8" ?>
+<rxui:ReactiveContentPage xmlns="http://xamarin.com/schemas/2014/forms"
+             xmlns:x="http://schemas.microsoft.com/winfx/2009/xaml"
+             xmlns:rxui="clr-namespace:ReactiveUI.XamForms;assembly=ReactiveUI.XamForms"
+             xmlns:vm="clr-namespace:TestSharp.Hi.ViewModels"
+             x:Class="TestSharp.Hi.Views.adminchickPage"
+             x:DataType="vm:adminchickViewModel"
+             x:TypeArguments="vm:adminchickViewModel">
+    <ContentPage.Content>
+        <StackLayout Padding="25">
+            <Text Text="{Binding RxText}"/>
+        </StackLayout>
+    </ContentPage.Content>
+</rxui:ReactiveContentPage>
